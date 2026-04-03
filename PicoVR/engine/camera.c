00d5f6281c329340da0f53eb521464de9f81958c
Patrick Dai <patrickdai917@gmail.com>
2026-04-02 22:34:02 -0700

    Cleaned and added some docs regarding view mat.

--- PicoVR/engine/camera.c
+++ PicoVR/engine/camera.c
@@ -28,16 +28,20 @@ mat4_t get_view_mat(const Camera_t *cam)
      * A camera's view matrix is computed by translating, then rotating
      * the world's coordinate frame to the camera's new coordinate frame.
      * As a result, we have
-     *          view_mat = transl_mat * rot_mat
+     *          view_mat = rot_mat * transl_mat
      *          transl_mat = [[1, 0, 0, -P.x],
                               [0, 1, 0, -P.y],
                               [0, 0, 1, -P.z],
                               [0, 0, 0,  1]]
-                rot_mat =    [[R_x, U_x, D_x, 0],
-     *                        [R_y, U_y, D_y, 0],
-     *                        [R_z, U_z, D_z, 0],
+                rot_mat =    [[R_x, R_y, R_z, 0],
+     *                        [U_x, U_y, U_z, 0],
+     *                        [D_x, D_y, D_z, 0],
      *                        [0, 0, 0, 1]]
-     * where each mat is column-major.
+     * We translate first to the eye location, then rotate around our new center.
+     * Let's say we rotated first. If we rotated first, then our translation would
+     * be using our new rotated axes, when our original translation is based on our
+     * original world frame. As a result, we translate along our original frame axes,
+     * then we rotate around our new world frame (rotating around new origin).s
      */
 
     mat4_t view_mat = {
@@ -45,8 +49,6 @@ mat4_t get_view_mat(const Camera_t *cam)
               {cam->u.x, cam->u.y, cam->u.z, 0.0f},
               {cam->d.x, cam->d.y, cam->d.z, 0.0f},
               {0.0f, 0.0f, 0.0f, 1.0f}}};
-    printf("View mat\n");
-    print_mat4(&view_mat);
     view_mat.m[0][3] = dot3((vec3_t *)&view_mat.m[0], &(vec3_t){-cam->p.x, -cam->p.y, -cam->p.z});
     view_mat.m[1][3] = dot3((vec3_t *)&view_mat.m[1], &(vec3_t){-cam->p.x, -cam->p.y, -cam->p.z});
     view_mat.m[2][3] = dot3((vec3_t *)&view_mat.m[2], &(vec3_t){-cam->p.x, -cam->p.y, -cam->p.z});
